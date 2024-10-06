b7a3a71691fbfa7c740e08529ce7f1ad98229919
GavrilS <gavr1lll@abv.bg>
2024-10-06 20:47:21 +0300

    Prepared the initial version for the runner script

--- runner.py
+++ runner.py
@@ -1,9 +1,37 @@
+import os
 from connectors.imap_connector import ImapConnector
 from mail_box_handlers.imap_handler import ImapHandler
 
-def main():
-    pass
+IMAP_USERNAME = os.getenv('IMAP_USER', None)
+IMAP_PASSWORD = os.getenv('IMAP_PASS', None)
+IMAP_SERVER = os.getenv('IMAP_SERVER', None)
+
+DEFAULT_MAIL_FOLDER = ''
+
+def main(handler, connector):
+    handler.get_messages(DEFAULT_MAIL_FOLDER)
+    connector.logout()
+
+
+def setup(creds):
+    connector = ImapConnector(
+        creds.get('username', None), 
+        creds.get('password', None), 
+        creds.get('imap_server', None)
+    )
+
+    connector.connect_to_mail_box()
+    handler = ImapHandler(connector.imap)
+
+    return handler, connector
 
 
 if __name__=='__main__':
-    pass
+    creds = {
+        'username': IMAP_USERNAME,
+        'password': IMAP_PASSWORD,
+        'imap_server': IMAP_SERVER
+    }
+
+    handler, connector = setup(creds)
+    main(handler, connector)
